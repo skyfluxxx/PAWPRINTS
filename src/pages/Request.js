05a7399c922af355aca252cfe563d229008743b3
skyfluxxx <spynxisros@gmail.com>
2024-01-11 22:52:41 +0800

    new changes to request

--- src/pages/Request.js
+++ src/pages/Request.js
@@ -1,11 +1,118 @@
-import React, { PureComponent } from 'react'
+import React, { useState } from 'react';
+import { firestore } from "../firebaseConfig";
+import { addDoc, collection } from "firebase/firestore";
+import '../components/Request.css';
 
-export class Request extends PureComponent {
-  render() {
-    return (
-      <div>Request</div>
-    )
-  }
-}
+const Request = () => {
+  const [formData, setFormData] = useState({
+    nameOfClient: '',
+    address: '',
+    telCellphone: '',
+    requestFor: '',
+    assistedBy: '',
+    dateAssisted: '',
+    remarks: '',
+  });
 
-export default Request
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    const requiredFields = ['nameOfClient', 'address', 'telCellphone', 'requestFor', 'assistedBy', 'dateAssisted', 'remarks'];
+
+    e.preventDefault();
+    const isAnyFieldEmpty = requiredFields.some((field) => formData[field].trim() === '');
+
+    if (isAnyFieldEmpty) {
+      alert('Please fill all necessary required fields!');
+      return;
+    }
+
+    console.log('Form submitted:', formData);
+
+    try {
+      const requestRef = collection(firestore, 'request');
+
+      await addDoc(requestRef, formData);
+      console.log('Form data stored in Firestore!');
+      alert('Form data stored in Firestore!');
+
+      setFormData({
+        nameOfClient: '',
+        address: '',
+        telCellphone: '',
+        requestFor: '',
+        assistedBy: '',
+        dateAssisted: '',
+        remarks: '',
+      });
+    } catch (error) {
+      console.error('Error storing form data:', error);
+      alert('Error storing form data:', error);
+    }
+  };
+
+  return (
+    <div className="request-form-container">
+      <h1>REPUBLIC OF THE PHILIPPINES</h1>
+      <h2>CITY OF LAPU-LAPU</h2>
+      <h3>OFFICE OF THE CITY VETERINARIAN</h3>
+      <p>Tel. No.: 341-2612</p>
+      <br />
+      <form onSubmit={handleSubmit} className="request-form">
+        <label>
+          Name of Client
+          <input type="text" name="nameOfClient" value={formData.nameOfClient} onChange={handleChange} />
+        </label>
+
+        <label>
+          Address
+          <input type="text" name="address" value={formData.address} onChange={handleChange} />
+        </label>
+
+        <label>
+          Tel/Cellphone No.
+          <input type="text" name="telCellphone" value={formData.telCellphone} onChange={handleChange} />
+        </label>
+
+        <label>
+          Request for:
+          <select name="requestFor" value={formData.requestFor} onChange={handleChange}>
+            <option value="" disabled selected>Select...</option>
+            <option value="Dog Vaccination">Dog Vaccination</option>
+            <option value="Dog Impounding">Dog Impounding</option>
+            <option value="Dog Surrender">Dog Surrender</option>
+            <option value="Others/Specify">Others/Specify</option>
+          </select>
+
+        </label>
+
+        <label>
+          Assisted By
+          <input type="text" name="assistedBy" value={formData.assistedBy} onChange={handleChange} />
+        </label>
+
+        <label>
+          Date Assisted
+          <input type="date" name="dateAssisted" value={formData.dateAssisted} onChange={handleChange} />
+        </label>
+
+        <label>
+          Remarks
+          <input type="text" name="remarks" value={formData.remarks} onChange={handleChange} />
+        </label>
+
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Request;
--- src/pages/Surrender.js
+++ src/pages/Surrender.js
@@ -1,11 +1,185 @@
-import React, { PureComponent } from 'react'
+import React, { useState } from 'react';
+import '../components/Surrender.css';
+import { firestore } from "../firebaseConfig";
+import { addDoc, collection } from "firebase/firestore";
 
-export class Surrender extends PureComponent {
-  render() {
-    return (
-      <div>Surrender</div>
-    )
-  }
-}
+const Surrender = () => {
+  const [formData, setFormData] = useState({
+    animalControlOfficer: '',
+    petName: '',
+    ownerName: '',
+    date: '',
+    placeFound: '',
+    address: '',
+    time: '',
+    ownerControlNo: '',
+    animalType: '',
+    dateTime: '',
+    age: '',
+    color: '',
+    veterinaryAttention: '',
+    releasingOfficer: '',
+    claimant: '',
+  });
 
-export default Surrender
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+
+    const requiredFields = [
+      'animalControlOfficer',
+      'petName',
+      'ownerName',
+      'date',
+      'placeFound',
+      'address',
+      'time',
+      'ownerControlNo',
+      'animalType',
+      'dateTime',
+      'age',
+      'color',
+      'veterinaryAttention',
+      'releasingOfficer',
+      'claimant',
+    ];
+
+    e.preventDefault();
+    const isAnyFieldEmpty = requiredFields.some((field) => formData[field].trim() === '');
+
+    if (isAnyFieldEmpty) {
+      alert('Please fill all necessary required fields!');
+      return;
+    }
+
+    console.log('Form submitted:', formData);
+
+    try {
+      const surrenderRef = collection(firestore, 'surrendered');
+
+      await addDoc(surrenderRef, formData);
+      console.log('Your inputs are successfully submitted!');
+      alert('Your inputs are successfully submitted!');
+
+      setFormData({
+        animalControlOfficer: '',
+        petName: '',
+        ownerName: '',
+        date: '',
+        placeFound: '',
+        address: '',
+        time: '',
+        ownerControlNo: '',
+        animalType: '',
+        dateTime: '',
+        age: '',
+        color: '',
+        veterinaryAttention: '',
+        releasingOfficer: '',
+        claimant: '',
+      });
+    } catch (error) {
+      console.error('Error encountered:', error);
+      alert('Error encountered:', error);
+    }
+  };
+
+  return (
+    <div className="surrender-form-container">
+      <h1>ANIMALS SURRENDER</h1>
+      <h2>OFFICE OF THE CITY VETERINARIAN</h2>
+      <h3>LAPU-LAPU CITY</h3>
+      <p>Tel. No.: 341-2612</p>
+      <br></br>
+      <form onSubmit={handleSubmit} className="surrender-form">
+        <label>
+          Animal Control Officer
+          <input type="text" name="animalControlOfficer" value={formData.animalControlOfficer} onChange={handleChange} />
+        </label>
+
+        <label>
+          Name of Pet
+          <input type="text" name="petName" value={formData.petName} onChange={handleChange} />
+        </label>
+
+        <label>
+          Owner's Name
+          <input type="text" name="ownerName" value={formData.ownerName} onChange={handleChange} />
+        </label>
+
+        <label>
+          Date
+          <input type="date" name="date" value={formData.date} onChange={handleChange} placeholder="dd/mm/yyyy" />
+        </label>
+
+        <label>
+          Place Found
+          <input type="text" name="placeFound" value={formData.placeFound} onChange={handleChange} />
+        </label>
+
+        <label>
+          Address
+          <input type="text" name="address" value={formData.address} onChange={handleChange} />
+        </label>
+
+        <label>
+          Time
+          <input type="time" name="time" value={formData.time} onChange={handleChange} placeholder="--:--" />
+        </label>
+
+        <label>
+          Owner's Control No.
+          <input type="text" name="ownerControlNo" value={formData.ownerControlNo} onChange={handleChange} />
+        </label>
+
+        <label>
+          Animal Type/Breed
+          <input type="text" name="animalType" value={formData.animalType} onChange={handleChange} />
+        </label>
+
+        <label>
+          Date & Time
+          <input type="text" name="dateTime" value={formData.dateTime} onChange={handleChange} placeholder="dd/mm/yyyy --:--" />
+        </label>
+
+        <label>
+          Age
+          <input type="text" name="age" value={formData.age} onChange={handleChange} />
+        </label>
+
+        <label>
+          Color
+          <input type="text" name="color" value={formData.color} onChange={handleChange} />
+        </label>
+
+        <label>
+          Veterinary Attention
+          <input type="text" name="veterinaryAttention" value={formData.veterinaryAttention} onChange={handleChange} />
+        </label>
+
+        <label>
+          Releasing Officer
+          <input type="text" name="releasingOfficer" value={formData.releasingOfficer} onChange={handleChange} />
+        </label>
+
+        <label className="claimant-section">
+          Claimant
+          <div className="claimant-input">
+            <input type="text" name="claimant" value={formData.claimant} onChange={handleChange} />
+          </div>
+        </label>
+
+        <br></br>
+        <button type="submit" className="submit-button">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Surrender;
